ba69e3cd95c477cdd282cd0a5bf8b2288129793a
Raul Lopez Gonzalez <raul.lopez@meltsan.com>
2021-11-22 13:04:38 -0600

    uso de fecha con thymeleaf

--- src/main/java/com/incocnitto/controller/HomeController.java
+++ src/main/java/com/incocnitto/controller/HomeController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String mostrarHome(Model model) {
         model.addAttribute("mensaje", "Bienvenido a Empleos app");
+        model.addAttribute("fecha", new Date());
         return "home";
     }
 
