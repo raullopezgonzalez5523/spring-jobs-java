d979eb594e3407fc0d3592d2d6b3c8286b365e23
Raul Lopez Gonzalez <raul.lopez@meltsan.com>
2021-11-22 13:27:21 -0600

    Expresion <th:each>

--- src/main/java/com/incocnitto/controller/HomeController.java
+++ src/main/java/com/incocnitto/controller/HomeController.java
@@ -5,9 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/listado")
+    public String mostrarListado(Model model) {
+        List<String> lista = List.of("Auxiliar de Contabilidad",
+                "Auxiliar de Administración",
+                "Auxiliar de Ventas",
+                "Auxiliar de Producción",
+                "Auxiliar de Calidad",
+                "Ingeniero",
+                "Arquitecto");
+        model.addAttribute("empleos", lista);
+        return "listado";
+    }
 
     @GetMapping("/")
     public String mostrarHome(Model model) {
